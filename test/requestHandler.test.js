acb517d0cc9894e72407909917410007479568a5
joshWyatt <joshuawyatt123@gmail.com>
2015-03-29 08:54:46 -0700

    Write working server test

---
+++ test/requestHandler.test.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var request = require('request');
+var test = require('tape-catch');
+
+var requestHandler = require('../requestHandler.js');
+var port = +process.argv.slice()[2] | 8001;
+
+
+test('requestHandler.js', function(t){
+
+  var server = http.createServer(requestHandler).listen(port, function(){
+    t.comment('Test server running');
+
+    request('http://localhost:' + port, function(err, res, body){
+      t.equal(body, 'heyo', 'It should send a response of \'heyo\'');
+
+      server.close();
+      t.comment('Test server shutting down');
+    });
+    
+  });
+
+  t.pass('It should pass the `pass` method');
+  t.ok(true, '`true` should be truthy');
+
+
+  t.end();
+});
