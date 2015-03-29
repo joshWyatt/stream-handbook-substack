7e7344b943c74347d8e8f7f4267fe4386e5c2b03
joshWyatt <joshuawyatt123@gmail.com>
2015-03-29 07:35:05 -0700

    Typo

--- test.config.js
+++ test.config.js
@@ -1,7 +1,7 @@
 var test = require('tape-catch');
 var path = require('path');
 
-test.createStream.pipe(process.stdout);
+test.createStream().pipe(process.stdout);
 
 process.argv.slice(2).forEach(function(file){
   require(path.resolve(file));
